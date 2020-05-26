5bb9d36c4b1ceaadaeb5027673c0d9758d4496da
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-26 19:34:43 -0400

    ready to begin coding set up sessions route and file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+    end
+
+
+
+
+    def user_params
+        require(:user).permit(username, password)
+    end
 end
