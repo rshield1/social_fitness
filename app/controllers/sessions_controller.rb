66aee72d9868980b5016eec4ba0c44e37cc925aa
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-27 00:34:39 -0400

    fixed login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
 
+def new
+   @user = User.new
+end
+
+def create
+    @user = User.find_by(id: params[:id])
+    if @user
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+    else
+        redirect_to "/login"
+    end
+end
+
 def destroy
     session.delete(:user_id)
     redirect_to '/'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
     def new
-        @user = User.new
     end
 
     def create
+        binding.pry
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
