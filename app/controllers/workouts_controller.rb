bcc3ca70795f01103c8cf335e7f366b4220e0098
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-27 02:45:16 -0400

    adding additional methods to workout controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,4 +3,21 @@ class WorkoutsController < ApplicationController
         @workout = Workout.new
         @workout.post.build
     end
+
+    def create 
+        @workout = Workout.new(workout_params)
+    if @workout.save
+        redirect_to workout_path(@workout)
+    else
+        render :new
+    end
+
+    end
+
+
+    private
+
+
+    def workout_params
+        params.require(:workout).permit(:name, :type, :sets, :reps, :difficulty, :post_id, post_attributes: [:title, :content])
 end
