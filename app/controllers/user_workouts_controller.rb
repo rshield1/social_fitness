90b68cadbbb90a6f6c26887fe0c0ac37a5e519f6
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-29 02:02:36 -0400

    looking at changing the join table name

--- app/controllers/user_workouts_controller.rb
+++ app/controllers/user_workouts_controller.rb
@@ -1,11 +1,15 @@
 class UserWorkoutsController < ApplicationController
 
     def new
+        @workout = Workout.find_by_id(params[:workout_id])
+     
+        @user_workout = @workout.build_user_workout
     end
 
     def index
+
     end
 
 
-    
+
 end
