97d5a808966ac31e1ea43f8e21946fe6896fd8ea
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-30 01:00:13 -0400

    added comments view and built form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+    def new
+        @workout = Workout.find_by_id(params[:workout_id])
+        @comment = @workout.comments.build
+    end
+
+
+    def index
+
+    end
+
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,10 @@ Rails.application.routes.draw do
   resources :moves
 
   resources :categories
+  resources :comments
   
   resources :workouts do
-    resources :categories, only [:new, :index]
+    resources :comments, only: [:new, :index]
   end
 
 end
