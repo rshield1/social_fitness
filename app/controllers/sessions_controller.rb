9798825b316155214c4e1fd84e30732af7f27384
Robert Shields <robert.shieldsiv@gmail.com>
2020-06-02 20:55:58 -0400

    fixed google omniauth in my sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,12 +20,17 @@ end
 
 
 def omniauth
-    binding.pry
-    User.find_or_create_by(email: auth[:info][:email])
-    
-
+    @user = User.find_or_create_by(email: auth[:info][:email]) do |u|
+        u.username = auth["info"]["name"]
+        u.password = SecureRandom.hex
+        
+    end   
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
 end
 
 def auth
     request.env['omniauth.auth']
+end
+
 end
