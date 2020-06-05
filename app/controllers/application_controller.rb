6fa50f2836c5cb975f1f8cbcfa48bb3438f0c4ec
Robert Shields <robert.shieldsiv@gmail.com>
2020-06-04 20:13:03 -0400

    set up user action so only user who matches session can edit or delete workout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   # :current_user_id This is a common way to handle user login in
   # a Rails application; logging in sets the session value and
   # logging out removes it.
+
   def current_user
-    @current_user ||= session[:current_user_id] &&
-      User.find_by(id: session[:current_user_id])
+    User.find_by(id: session[:user_id])
   end
 
   def require_login
